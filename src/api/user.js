36d8d2b3b03b346f803d1347f6b16e4391c31814
MomentRing <904372358@qq.com>
2022-06-26 18:04:39 +0800

    完成登录功能

---
+++ src/api/user.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+/**
+ *获取短信验证码
+ * @param {*} mobile
+ * @returns
+ */
+export const getSmsCode = (mobile) => {
+  return request({
+    url: `/sms/codes/${mobile}`
+  })
+}
+/**
+ *登陆
+ * @param {*} param0
+ * @returns
+ */
+export const login = ({ mobile, code }) => {
+  return request({
+    method: 'post',
+    url: '/authorizations',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
